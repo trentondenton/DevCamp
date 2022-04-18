770c1a096ef54552240139b8d55962616bbde76a
Trenton <trentondenton91@gmail.com>
2022-04-18 10:57:51 -0500

    Updating Repositories

---
+++ LearningTypeScript/005_typescript_config.js
@@ -0,0 +1,200 @@
+"use strict";
+// function hey_there() {
+//   console.log("Hi from typescript")
+// }
+// var fullName: string = "Trenton Denton";
+// let paidAccount: boolean = true;
+// const versionNumber: number = 1.3;
+// function printName(f: string, l: string) {
+//   var greeting: string = "Hi there, ";
+//   console.log(greeting + f + " " + l);
+// }
+// console.log(fullName);
+// console.log(paidAccount);
+// console.log(versionNumber);
+// hey_there();
+// printName("Trenton", "Denton");
+// var msg: string = "Trenton";
+// console.log("A long message to " + msg + " filled with text");
+// console.log(`A long message to ${msg} filled with text`);
+// // Booleans
+// let paidAccount: Boolean = false;
+// // Numbers
+// let age: number = 33;
+// var taxRate: number = 7.5;
+// // Strings
+// var fullName: string = "Trenton Denton";
+// // Arrays
+// var ages: number[] = [33, 28, 11];
+// // Tuples
+// let player: [number, string, number, number];
+// player = [3, "Corerra", .333, 33];
+// // Enum
+// enum ApprovalStatus { Approved, Pending, Rejected };
+// let job: ApprovalStatus = ApprovalStatus.Pending;
+// // Any
+// var apiData: any[] = [123, "Trenton", false];
+// //Void
+// function printout(msg: string): void {
+//   console.log(msg);
+// }
+// // Type Aliases
+// type PlayerArray = Array<string>;
+// let players: PlayerArray = ["Altuve", "Correra", "Bregman"];
+// console.log(players);
+// // Union Types
+// type PlayerArray = Array<string | number>;
+// let players: PlayerArray = ["Altuve", "Correra", "Bregman"];
+// let playerNumbers: PlayerArray = [25, 3, 2]
+// console.log(players);
+// console.log(playerNumbers);
+// var gradeGenerator(grade: number): string => {
+//   if (grade < 60) {
+//     return 'F';
+//   }
+//   if (grade >= 60 && grade < 70) {
+//     return 'D';
+//   }
+//   if (grade >= 70 && grade < 80) {
+//     return 'C';
+//   }
+//   if (grade >= 80 && grade < 90) {
+//     return 'B';
+//   }
+//   if (grade >= 90) {
+//     return 'A';
+//   }
+// }
+// gradeGenerator(75);
+// var names: string[] = ['Trenton', 'Koda', 'Kaldr'];
+// var counter: number = 0;
+// (function () {
+//   for (let name in names) {
+//     counter++;
+//   }
+// })();
+// console.log(counter);
+// var fullName: (first: string, last: string) => string;
+// fullName = function (first: string, last: string) {
+//   return first + " " + last;
+// }
+// console.log(fullName('Trenton', 'Denton'));
+// (function (first: string, last: string) {
+//   console.log(first + " " + last);
+// })('Kaldr', 'Denton');
+//Variables have access to any public variables in the outer scope
+// function nameFunction(name: string): void {
+//   var n: string = name;
+//   function printName() {
+//     console.log(n);
+//   }
+//   printName();
+// }
+// nameFunction('Trenton');
+// function lineup() {
+//   var nowBatting: number = 1;
+//   return {
+//     nextBatter() { nowBatting++ },
+//     currentBatter() { return nowBatting }
+//   }
+// }
+// let batters = lineup();
+// console.log(batters.currentBatter());
+// batters.nextBatter();
+// console.log(batters.currentBatter());
+// batters.nextBatter();
+// console.log(batters.currentBatter());
+// interface User {
+//   email: string;
+//   firstName?: string;
+//   lastName?: string;
+// }
+// class Admin {
+//   role: string;
+//   constructor(public email: string) {
+//     this.role = 'Admin';
+//   }
+// }
+// function profile(user: User): string {
+//   return `Welcome, ${user.email}`;
+// }
+// var joe = new Admin('joe@example.com');
+// console.log(joe.role);
+// interface IPost {
+//   title: string;
+//   body: string;
+// }
+// class Post implements IPost {
+//   title: string;
+//   body: string;
+//   constructor(post: IPost) {
+//     this.title = post.title;
+//     this.body = post.body;
+//   }
+//   printPost() {
+//     console.log(this.title);
+//     console.log(this.body);
+//   }
+// }
+// var post = new Post({ title: "My Great Title", body: "Some content" });
+// console.log(post.title);
+// console.log(post.body);
+// post.printPost();
+// namespace Post {
+//   export interface IPost {
+//     title: string;
+//     body: string;
+//     slug: string;
+//     seoKeywords: string;
+//   }
+//   export class Post implements IPost {
+//     title: string;
+//     body: string;
+//     slug: string;
+//     seoKeywords: string
+//     constructor(post: IPost) {
+//       this.title = post.title;
+//       this.body = post.body;
+//       this.slug = post.slug;
+//       this.seoKeywords = post.seoKeywords;
+//     }
+//     printPost() {
+//       console.log(this.title);
+//       console.log(this.body);
+//       console.log(this.slug);
+//       console.log(this.seoKeywords);
+//     }
+//   }
+// }
+// var blogPost = new Post.Post({
+//   title: "My great post",
+//   body: "some content",
+//   slug: "my-great-post",
+//   seoKeywords: "any, words"
+// });
+// blogPost.printPost();
+// interface User { email: string; firstName?: string; lastName?: string; } function profile(user: User): string { return `Welcome, {user.email}`; }
+// var realUser = { email: 'test@test.com', firstName: 'Jordan', lastName: 'Hudgens', sayHi() { return "Hey there!"; } };
+// console.log(realUser.email);
+// class Invoice { companyProfile: string; constructor(public name: string, public city: string, public state: string) { this.companyProfile = name + ", " + city + ", " + state; } }
+// var testInvoice = new Invoice('company name', 'city', 'State');
+// console.log(testInvoice.name, testInvoice.city, testInvoice.state)
+var Invoice = /** @class */ (function () {
+    function Invoice(total) {
+        this.total = total;
+    }
+    Invoice.prototype.printTotal = function () {
+        console.log(this.total);
+    };
+    Invoice.prototype.printLater = function (time) {
+        var _this = this;
+        setTimeout(function () {
+            console.log(_this.total);
+        }, time);
+    };
+    return Invoice;
+}());
+var invoice = new Invoice(400);
+invoice.printTotal();
+invoice.printLater(1000);
+//# sourceMappingURL=005_typescript_config.js.map
